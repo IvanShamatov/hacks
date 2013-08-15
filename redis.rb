f826eb82b90bbee816d8b477a9ce461af84bf61d
IvanShamatov <status.enable@gmail.com>
2013-08-16 01:28:10 +0400

    redis added

--- redis.rb
+++ redis.rb
@@ -7,14 +7,14 @@ Redis.current
 
 # or wrap it in method
 def redis
-	Redis.current
+  Redis.current
 end
 
 
 ##
 # Delete all records in redis by wildcard
 def delete_by(wildcard)
-	redis.keys(wildcard).each do |key|
-		redis.del(key)
-	end
+  redis.keys(wildcard).each do |key|
+    redis.del(key)
+  end
 end
